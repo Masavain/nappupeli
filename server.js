c8fe64705e2c670a9bfaf101d0c593002aa540c9
Masavain <matti.vainionpaa@helsinki.fi>
2019-02-10 23:17:12 +0200

    second chance

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const middleware = require('./utils/middleware')
 const counterRouter = require('./controllers/counter')
 const winnersRouter = require('./controllers/winners')
 
-const port = 4001
-const url = 'mongodb://Masavain:Nappipeli123@ds223605.mlab.com:23605/nappipeli'
+const config = require('./utils/config')
 
 const app = express()
-const server = app.listen(4001)
 
-mongoose.connect(url)
+mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
 app.use(cors())
@@ -26,7 +24,7 @@ app.use('/api/winners', winnersRouter)
 app.use(middleware.logger)
 app.use(middleware.error)
 
-
+const server = http.createServer(app)
 var io = require('socket.io').listen(server);
 
 io.on('connection', socket => {
@@ -42,4 +40,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(config.port, () => console.log(`Listening on port ${config.port}`))
