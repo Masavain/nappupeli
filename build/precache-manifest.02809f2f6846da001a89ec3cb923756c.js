b816cee73b25d5dded115aa287447dcb7ad42a35
Masavain <matti.vainionpaa@helsinki.fi>
2019-02-10 23:35:46 +0200

    joko ny

---
+++ build/precache-manifest.02809f2f6846da001a89ec3cb923756c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bb1421b9db7b8ead6c1",
+    "url": "/static/js/main.9bb1421b.chunk.js"
+  },
+  {
+    "revision": "48ce8a753b5db8339ce1",
+    "url": "/static/js/1.48ce8a75.chunk.js"
+  },
+  {
+    "revision": "9bb1421b9db7b8ead6c1",
+    "url": "/static/css/main.4f9958a2.chunk.css"
+  },
+  {
+    "revision": "dd5a4e17a5cc8af6a2a38fb69b872149",
+    "url": "/index.html"
+  }
+];
